503c1e60bacd72251b72838b3d989f78e2ea9b5c
SeanErvinson <ervinsonong@gmail.com>
2019-04-06 09:35:04 +0800

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Genetic_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
